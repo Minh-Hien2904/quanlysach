14b88bd4efcdad8cf7f77fff1752501f67356127
Minh Hiển <hientrithuc58@gmail.com>
2025-05-12 14:59:57 +0700

    thứ 2 ngày 12/05/2025

--- quanlysach/demo/src/main/java/com/example/quanlysach/model/User.java
+++ quanlysach/demo/src/main/java/com/example/quanlysach/model/User.java
@@ -1,4 +1,41 @@
 package com.example.quanlysach.model;
 
 public class User {
+    // Các thuộc tính của User
+    private String username;
+    private String password;
+
+    // Constructor mặc định (không tham số)
+    public User() {
+        // Khởi tạo mặc định, có thể để trống hoặc gán giá trị mặc định
+    }
+
+    // Constructor có tham số (nếu cần)
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getter và Setter cho các thuộc tính
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // Phương thức khác (nếu cần)
+    public void displayUserInfo() {
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+    }
 }
